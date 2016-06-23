72ea716588f256ff002c14ca9ff6979f53057d15
Michael Cook Ryan <michaelcookryan@users.noreply.github.com>
2016-06-23 15:51:52 -0700

    refactored code removal
    removed var_dump() and commented out code

--- display.php
+++ display.php
@@ -24,8 +24,8 @@
 		DATE: 8-29-2015
 		FILE: record.php  
 		FILE DESCRIPTION: This file will display all information of the selected pre-existing file. 
-							This is simply for display out put or an option to print the record.
-							No counters are started.
+							As well give option to print the record, return to main list page, or 
+							start time on current selection.
 							
  */
 
@@ -87,55 +87,7 @@ function startTheClockForToday()
 
 if(!empty($_POST['active']) || !empty($_POST['n'])) // confirm that something was selected or created ... fails if both empty
 {
-	
-
-
-	/*if(!empty($_POST['n']) && empty($_POST['active'])) // this is a new client so create a new client file   // remove frome here
-	{
-		
-
-		$newClientInfo = $_POST['n'] . "," . $_POST['p'] . "," . date("D. M j Y") . "," . "nil";
-		$newFilePath =  "clientDirectory/" . $_POST['n'] . ".txt";
-
-		if(!file_exists($newFilePath)) // prevent appending duplicates onto existing files    
-		{
-			$newClientFile = @fopen($newFilePath, "a+") or die("No file exists for this client.");
-
-		
-				fwrite($newClientFile, $newClientInfo . PHP_EOL);
-
-				fclose($newClientFile);
-
-				
-				startTheClockForToday();
-
-				$_SESSION['currentClientWork'] = $_POST['n'];  // for display use in endwork.php and logout.php
-
-				echo "Client: " . $_POST['n'] . "<br>";
-				echo "Project: " . $_POST['p'] . "<br>";
-				echo "Start Date: " . date("D. M j Y");
-
-				echo "<form method='post' action='endwork.php'>
-
-				<input 
-                type='submit' 
-                name='startTimer' 
-                value='End Timer'> 
-
-
-		</form>";
-
-
-
-			}else{
-
-				echo "A file with this name already exists.<br>";
-				echo "<a href='index.php'>Back to Main Page</a>";
-				exit;
-			}	
-	
-	
-	}else */if(!empty($_POST['active']) && is_null($_POST['n']))  // recalling existing file and listing it's logged info into a table
+	if(!empty($_POST['active']) && is_null($_POST['n']))  // recalling existing file and listing it's logged info into a table
 	{
 		$startThisFileAgain = urlencode($_POST['active']); //used to pass into input field below when calling record.php
 		//startTheClockForToday();
@@ -182,7 +134,7 @@ if(!empty($_POST['active']) || !empty($_POST['n'])) // confirm that something wa
 			
 
 
-				//$_SESSION['projectTotalTimeFromLastUpdate'] = $item[3];		// used by logout.php to calculate the overall total time on the project
+				
 
 				$runningTotal = totalTime($item[3]);  // converts to time format for display
 
@@ -221,17 +173,9 @@ if(!empty($_POST['active']) || !empty($_POST['n'])) // confirm that something wa
 
 
 		</form>";
-		//var_dump($startThisFileAgain);
-		var_dump($_POST['n']);
-		var_dump($_POST['active']);
-
-	}else if(!empty($_POST['active']) && isset($_POST['n']))  // both a new file and existing file are selected
 		
-		{
-			echo "You may only select an existing file or start a new one...not both.<br>";		// THIS SECTION SHOULD BE REMOVED AS THERE IS NO LONGER A CHANCE TO BE ABLE TO SELECT BOTH
-			echo "<a href='current.php'>Back to Client List Page</a>";
-			exit;
-		}
+
+	}
 
 }else{ // nothing entered or selected
 
